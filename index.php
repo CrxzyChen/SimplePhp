180dada37b837ee63d83c946e113a51a941749f8
Crxzy <crxzy@outlook.com>
2020-01-11 00:40:35 +0800

    SimplePhp version 0.0.0.1

--- index.php
+++ index.php
@@ -8,10 +8,12 @@ define("LOCAL_ROOT", $_SERVER["DOCUMENT_ROOT"] . dirname($_SERVER["REQUEST_URI"]
 define("HTTP_ROOT", $protocol . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']));
 define("SIMPLEPHP_DIR", LOCAL_ROOT . DIRECTORY_SEPARATOR . "SimplePhp");
 define("DRIVERS_DIR", LOCAL_ROOT . DIRECTORY_SEPARATOR . "Drivers");
+
 //start session
 if (!isset($_SESSION)) {
     session_start();
 }
+
 //load class Exception
 if (!file_exists(SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php")) {
     die("Exception.json is not exist! Exception.php:" . SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php");
@@ -19,9 +21,11 @@ if (!file_exists(SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php")) {
     require_once SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php";
 }
 
+//load handle method of exception
 set_error_handler("\SimplePhp\Exception::error_handler", E_ALL);
 set_exception_handler("\SimplePhp\Exception::exception_handler");
 spl_autoload_register("\SimplePhp\Exception::autoload_register");
+
 //load config.json
 if (!file_exists(LOCAL_ROOT . DIRECTORY_SEPARATOR . "config.json")) {
     throw new \SimplePhp\Exception("config.json is not exist! config.json: " . LOCAL_ROOT . DIRECTORY_SEPARATOR . "config.json");
