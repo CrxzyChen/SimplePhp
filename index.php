8573dacd82ed6be6b8ce63dbe48955ff973d8474
Crxzy <crxzy@outlook.com>
2020-01-11 10:06:06 +0800

    SimplePhp version 0.0.0.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 echo "<pre>";
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
-define("LOCAL_ROOT", $_SERVER["DOCUMENT_ROOT"] . dirname($_SERVER["REQUEST_URI"]));
+define("LOCAL_ROOT", $_SERVER["DOCUMENT_ROOT"] . dirname($_SERVER["SCRIPT_NAME"]));
 define("HTTP_ROOT", $protocol . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']));
 define("SIMPLEPHP_DIR", LOCAL_ROOT . DIRECTORY_SEPARATOR . "SimplePhp");
 define("DRIVERS_DIR", LOCAL_ROOT . DIRECTORY_SEPARATOR . "Drivers");
@@ -13,10 +13,9 @@ define("DRIVERS_DIR", LOCAL_ROOT . DIRECTORY_SEPARATOR . "Drivers");
 if (!isset($_SESSION)) {
     session_start();
 }
-
 //load class Exception
 if (!file_exists(SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php")) {
-    die("Exception.json is not exist! Exception.php:" . SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php");
+    die("Exception.php is not exist! Exception.php:" . SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php");
 } else {
     require_once SIMPLEPHP_DIR . DIRECTORY_SEPARATOR . "Exception.php";
 }
