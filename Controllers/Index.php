f94973380e161fa011cea35edf632bdd6b65f02d
Crxzy <crxzy@outlook.com>
2020-01-30 23:28:40 +0800

    update

--- Controllers/Index.php
+++ Controllers/Index.php
@@ -11,20 +11,10 @@ namespace Controllers;
 
 class Index extends ControllerBase
 {
-    private $image_cloud;
-
     protected function onCreate()
     {
-        $this->image_cloud = new \Models\ImageCloud();
         // TODO: Implement onCreate() method.
     }
 
-    public function Index()
-    {
-        $image_resource = $this->image_cloud->getImageResource($_GET["gallery"], $_GET["image_name"], $_GET["image_form"]);
-        return $image_resource;
-//        header('Content-Type: image/gif');
-//        imagegif($image_resource);
-//        imagedestroy($image_resource);
-    }
+    public function Index(){}
 }
